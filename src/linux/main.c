59e3e42307acadc036c11aee3c447f91cde171ee
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-02-14 15:04:49 -0300

    bibliotecas externas agregadas a engine

--- src/linux/main.c
+++ src/linux/main.c
@@ -3,13 +3,7 @@
 int main(void)
 {
 
-#ifdef D_WIN32
-    printf("Windows\n");
-#elif defined(D_LINUX)
-    printf("Linux\n");
-#else
-    printf("Desconhecido\n");
-#endif
+
 
     return 0;
 }
--- src/win32/main.c
+++ src/win32/main.c
@@ -8,13 +8,6 @@ int WINAPI WinMain(
     LPSTR lpCmdLine,
     int nCmdShow)
 {
-#ifdef D_WIN32
-    printf("Windows\n");
-#elif defined(D_LINUX)
-    printf("Linux\n");
-#else
-    printf("Desconhecido\n");
-#endif
 
     return 0;
 }
