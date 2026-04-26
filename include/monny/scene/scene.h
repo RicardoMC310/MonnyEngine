9ef8438a0a035238db58f0adb8651a18e79ac391
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-04-25 23:17:12 -0300

    feat: add scene_set_background function

--- include/monny/scene/scene.h
+++ include/monny/scene/scene.h
@@ -3,12 +3,17 @@
 
 #include <monny/types.h>
 #include <monny/renderer/renderer.h>
+#include <monny/script/script.h>
 
-typedef struct scene_t {
+typedef struct scene_manager_t scene_manager_t;
+
+typedef struct scene_t
+{
     char name[64];
-    f32 r,g,b;
+    f32 r, g, b;
 } scene_t;
 
 void scene_render(scene_t *scene, renderer_t *renderer);
+void scene_set_background(scene_t *scene, f32 r, f32 g, f32 b);
 
 #endif
--- src/linux/scene/scene.c
+++ src/linux/scene/scene.c
@@ -1,17 +1,30 @@
 #include <monny/scene/scene.h>
 #include <monny/renderer/renderer.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+
 // struct scene_t {
 
 // };
 
 void scene_render(scene_t *scene, renderer_t *renderer)
 {
-    renderer_command_t cmd = {0};
+    renderer_command_t cmd;
+    // if (!cmd) return;
 
-    cmd.bg_color[0] = scene->r;
-    cmd.bg_color[1] = scene->g;
-    cmd.bg_color[2] = scene->b;
+    cmd.r = scene->r;
+    cmd.g = scene->g;
+    cmd.b = scene->b;
 
     renderer_submit(renderer, &cmd);
+}
+
+void scene_set_background(scene_t *scene, f32 r, f32 g, f32 b)
+{
+    if (!scene) return;
+
+    scene->r = r;
+    scene->g = g;
+    scene->b = b;
 }
