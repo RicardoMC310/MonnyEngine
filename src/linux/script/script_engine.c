91ee13aeffb395afb44a0d9bc879a32352438337
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-04-25 23:17:33 -0300

    feat: convert engine script to module registry system

--- src/linux/script/script_engine.c
+++ src/linux/script/script_engine.c
@@ -1,20 +1,31 @@
 #include <monny/script/script.h>
 #include <monny/core/engine.h>
 
-int l_engine_get_fps(script_wt *wt) {
+static int l_engine_get_fps(script_wt *wt)
+{
     engine_t *engine = script_get_context(wt);
 
     script_pushnumber(wt, engine_get_fps(engine));
     return 1;
 }
 
-void engine_script_register(script_t *script, engine_t *engine)
+static void engine_script_register(script_t *script, engine_t *engine)
 {
     script_register_function(
-        script, 
+        script,
         "engine",
         "get_fps",
         l_engine_get_fps,
-        engine
-    );
+        engine);
 }
+
+static void engine_module_resgiter(script_t *script, engine_t *engine)
+{
+    engine_script_register(script, engine);
+}
+
+script_module_t engine_module = {
+    .name = "engine",
+    .registry_func = engine_module_resgiter};
+
+REGISTER_MODULE(engine_module)
